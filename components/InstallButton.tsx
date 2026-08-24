e01febfe1f5ed0861dc10eb7724e7a96907644b4
milind-soni <milindsoni201@gmail.com>
2026-08-24 23:25:18 +0530

    Use the real OpenMausBot install icon

--- components/InstallButton.tsx
+++ components/InstallButton.tsx
@@ -3,7 +3,7 @@
 export default function InstallButton({ installUrl }: { installUrl: string }) {
   return (
     <a className="button button-primary install-button" href={installUrl}>
-      <img src="/icon.svg" width="20" height="20" alt="" />
+      <img src="/app-icon.png" width="20" height="20" alt="" />
       Add to OpenMausBot
       <span aria-hidden="true">↗</span>
     </a>
